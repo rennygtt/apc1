d189ab36fde1ce9add6e4d13668148a87296ce6c
rennygtt <rennyfreitas0@gmail.com>
2024-10-02 12:29:06 +0000

    sexo

---
+++ pratica/pratica03/declara_livro.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+char isbn[14] = "0000000000000";
+int num_paginas = 000;
+float preco = 0000.00
+int ano_publicacao = 0000;
+//
+ printf("ISBN %S\N", isbn);
+ printf("Num. Paginas %03d\n", num_paginas);
+ printf("Preco R$ %.2f\n", preco);
+ printf("publicado em: %04d\n", ano_publicacao);
+//
+ ISBN:0000000000000;
+ num. paginas:000; 
+ preco : R$0000.00;
+ publicado : 0000; 
+  
+
+
+
+  return 0; 
+}
